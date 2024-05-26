a9435ad2efd619cbeace987c06e8ae4b9b1e87d1
Andre <andrebp1020@gmail.com>
2024-05-25 21:43:54 -0500

    todo en orden, falta activar el boton con el color

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,6 +1,4 @@
 import Link from "next/link"
-import React from 'react'
-
 
 // iconos
 import { FaCalculator } from "react-icons/fa";
@@ -10,6 +8,12 @@ const menuItems=[
     {
         path:"/dashboard/main",
         title: "Main",
+        icon: <FaCalculator/>,
+
+    },
+    {
+        path:"/dashboard/counter",
+        title: "Counter",
         icon: <FaCalculator/>,
 
     }
@@ -22,7 +26,7 @@ export const Sidebar = () => {
     <aside className="bg-gradient-to-br from-gray-800 to-gray-900 -translate-x-80 inset-0 z-50 my-4 ml-4 h-[calc(100vh-32px)] w-72 rounded-xl transition-transform duration-300 xl:translate-x-0">
       <div className="relative border-b border-white/20">
         <Link className="flex items-center gap-4 py-6 px-8" href="#/">
-          <h6 className="block antialiased tracking-normal font-sans text-base font-semibold leading-relaxed text-white">Material Tailwind Dashboard</h6>
+          <h6 className="block antialiased tracking-normal font-sans text-base font-semibold leading-relaxed text-white">My Dashboard</h6>
         </Link>
         <button className="middle none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-8 max-w-[32px] h-8 max-h-[32px] rounded-lg text-xs text-white hover:bg-white/10 active:bg-white/30 absolute right-0 top-0 grid rounded-br-none rounded-tl-none xl:hidden" type="button">
         </button>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
+import { redirect } from "next/navigation";
 
 export default function HomePage() {
   return (
-  <div className="flex flex-row bg-slate-100">
-    
-    <h1 className="text-black text-7xl ">hola mundo</h1>
-  </div>
+    redirect("/dashboard/main")
   );
 }
